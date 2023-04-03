6ed56c03e6fbbe2af2761c1eeaacbba08b254946
Doreapp <doreapp@github.com>
2023-04-03 20:30:44 +0200

    :hammer: Introduce room parsing

---
+++ scrapaddon/airbnb/api.js
@@ -0,0 +1,116 @@
+/**
+ * Wrapper to call Airbnb API
+ */
+
+/**
+ * Wrapper object to interact with Airbnb API
+ */
+class API {
+
+    /**
+     * Builds the wrapper
+     * @param {string} apiKey Airbnb API key, easily gettable. Anyone can use d306zoyjsyarp7ifhu67rjxn52tv0t20.
+     * @param {string} currency Code for the currency to use. E.g. EUR, USD.
+     * @param {string} locale Code for the language to use, e.g. fr, en.
+     * @param {string} host URL host. Defaults to www.airbnb.fr
+     */
+    constructor(apiKey, currency = "EUR", locale = "fr", host = "www.airbnb.fr") {
+        this.baseUrl = `https://${host}/api/v3`
+        this.apiKey = apiKey
+        this.currency = currency
+        this.locale = locale
+    }
+
+    /**
+     * Perform a GET request to the API
+     * @param {string} url Full URL to GET
+     * @param {object} data Optional data object to send
+     * @returns HTTP response
+     */
+    async get(url, data = undefined) {
+        const options = {
+            headers: {
+                "x-airbnb-api-key": this.apiKey
+            }
+        }
+        if (data !== undefined) {
+            options.body = JSON.stringify(data)
+        }
+        return await fetch(url, options)
+    }
+
+    /**
+     * Fetches `Stays Pdp Section` endpoint
+     * @param {{id: string, query: object}} options
+     *  `id` is the id of the room (foundable in the URL as `www.airbnb.fr/room/<id>`)
+     *  `query` is the query object to send, containing the URL parameters (e.g. checkout, checkin)
+     * @returns HTTP Response instance
+     */
+    async getStaysPdpSection({ id, query }) {
+        const endpoint = "StaysPdpSections"
+        const urlQuery = {
+            operationName: "StaysPdpSections",
+            locale: this.locale,
+            currency: this.currency,
+        }
+        urlQuery.variables = {
+            id: btoa(`StayListing:${id}`),
+            pdpSectionsRequest:
+            {
+                adults: query.adults || 1,
+                bypassTargetings: false,
+                categoryTag: query.category_tag || null,
+                causeId: null,
+                children: query.children || 0,
+                disasterId: null,
+                discountedGuestFeeVersion: null,
+                displayExtensions: null,
+                federatedSearchId: query.federated_search_id || null,
+                forceBoostPriorityMessageType: null,
+                infants: query.infants || 0,
+                interactionType: null,
+                layouts: ["SIDEBAR", "SINGLE_COLUMN"],
+                pets: query.pets || 0,
+                pdpTypeOverride: null,
+                preview: false,
+                previousStateCheckIn: null,
+                previousStateCheckOut: null,
+                priceDropSource: null,
+                privateBooking: false,
+                promotionUuid: null,
+                relaxedAmenityIds: null,
+                searchId: null,
+                selectedCancellationPolicyId: null,
+                selectedRatePlanId: null,
+                splitStays: null,
+                staysBookingMigrationEnabled: false,
+                translateUgc: null,
+                useNewSectionWrapperApi: false,
+                sectionIds: ["BOOK_IT_CALENDAR_SHEET", "POLICIES_DEFAULT", "BOOK_IT_SIDEBAR", "URGENCY_COMMITMENT_SIDEBAR", "BOOK_IT_NAV", "BOOK_IT_FLOATING_FOOTER", "EDUCATION_FOOTER_BANNER", "URGENCY_COMMITMENT", "EDUCATION_FOOTER_BANNER_MODAL", "CANCELLATION_POLICY_PICKER_MODAL"],
+                checkIn: query.check_in || null,
+                checkOut: query.check_out || null,
+                p3ImpressionId: query.source_impression_id || null,
+                photoId: null
+            }
+        }
+        urlQuery.extensions = {
+            persistedQuery: {
+                version: 1,
+                sha256Hash: "69cb84c3d27f18f34306c9f83f19064035080ae6d8fa803935860843f2bfb570"
+            }
+        }
+        this.sanatizeQuery(urlQuery)
+        const searchParams = new URLSearchParams(urlQuery)
+        return await this.get(`${this.baseUrl}/${endpoint}?${searchParams.toString()}`)
+    }
+
+    /**
+     * Sanatize query object, transforming `variables` and `extensions` attributes
+     * as JSON string
+     * @param {{variables: object, extensions: object}} query Query object to sanatize
+     */
+    sanatizeQuery(query) {
+        query.variables = JSON.stringify(query.variables)
+        query.extensions = JSON.stringify(query.extensions)
+    }
+}
--- scrapaddon/airbnb/main.js
+++ scrapaddon/airbnb/main.js
@@ -1,21 +1,201 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/write
+/**
+ * Main script of the extension
+ */
 
 
-(function () {
-    const DEBUG = true
+const DEBUG = true
+const API_KEY = "d306zoyjsyarp7ifhu67rjxn52tv0t20"
 
-    const log = (...data) => {
-        if (DEBUG) {
-            console.log("[Malt scrap]", ...data)
+/**
+ * Log information (if DEBUG is enabled)
+ * @param  {...any} data
+ */
+const log = (...data) => {
+    if (DEBUG) {
+        console.log("[Malt scrap]", ...data)
+    }
+}
+
+/**
+ * Parse `BookIt` section
+ * @param {object} section Section object
+ * @returns {{price?: string}} Parsed data
+ */
+const parseBookItSection = (section) => {
+    if (!section.structuredDisplayPrice)
+        return {}
+    if (!section.structuredDisplayPrice.primaryLine)
+        return {}
+    const price = section.structuredDisplayPrice.primaryLine.price.replace(/\s/, "")
+    return { price }
+}
+
+/**
+ * Parse a section
+ * @param {object} section Section object to parse
+ * @returns {{price?: string}} Parsed data
+ */
+const parseSection = (section) => {
+    if (!section)
+        return {}
+    switch (section.__typename) {
+        case "SectionContainer":
+            return parseSection(section.section)
+        case "BookItSection":
+            return parseBookItSection(section)
+        default:
+            log("Unable to parse section.", `typename=${section.__typename}`, section)
+    }
+    return {}
+}
+
+/**
+ * Parses the stays pdp section, return in the request response
+ * @param {object} data Response data
+ * @returns Parsed data
+ */
+const parseStaysPdpSection = (data) => {
+    const result = {}
+    log("Parsing data...", data)
+    const meaningful = data.data.presentation.stayProductDetailPage.sections.metadata
+    const enventDataLoggingKeys = [
+        "accuracyRating",
+        "amenities",
+        "bedType",
+        "checkinRating",
+        "cleanlinessRating",
+        "communicationRating",
+        "guestSatisfactionOverall",
+        "homeTier",
+        "isSuperhost",
+        "listingId",
+        "listingLat",
+        "listingLng",
+        "locationRating",
+        "personCapacity",
+        "pictureCount",
+        "roomType",
+        "valueRating",
+        "visibleReviewCount"
+    ]
+    for (const key of enventDataLoggingKeys) {
+        result[key] = meaningful.loggingContext.eventDataLogging[key]
+    }
+    const sharingConfigKeys = [
+        "imageUrl",
+        "location",
+        "pdpLink",
+        "personCapacity",
+        "propertyType",
+        "reviewCount",
+        "starRating",
+        "title"
+    ]
+    for (const key of sharingConfigKeys) {
+        result[key] = meaningful.sharingConfig[key]
+    }
+    for (const section of data.data.presentation.stayProductDetailPage.sections.sections) {
+        for (const [key, value] of Object.entries(parseSection(section))) {
+            result[key] = value
         }
     }
+    return result
+}
 
-    button = Scrap.addScrapButton()
-
-    button.onclick = () => {
-        log("Scrapping starts")
+/**
+ * Parse URL, extracting query params and id
+ * @returns {{url: string, id: string, query: object}} Parsed data
+ */
+const parseUrl = () => {
+    const url = window.location.href
+    const splits = url.split("?")
+    const baseName = splits[0].split("/").splice(-1)[0]
+    const result = {
+        url,
+        id: baseName,
+        query: {}
     }
-    log("Ok")
-})()
+    const urlQuery = splits.slice(1).join("?")
+    const params = new URLSearchParams(urlQuery)
+    params.forEach((value, key) => {
+        result.query[key] = value
+    })
+    return result
+}
 
+/**
+ * Get the real price of the room, using `/book/stays/` page
+ * @param {{id: string, query: object}} options URL data query and id
+ * @returns Parsed data
+ */
+const getRealPrice = async ({ id, query }) => {
+    const newQuery = {
+        numberOfAdults: query.adults || 0,
+        numberOfChildren: query.children || 0,
+        numberOfInfants: query.infants || 0,
+        numberOfPets: query.pets || 0,
+        checkin: query.check_in,
+        checkout: query.check_out,
+        guestCurrency: "EUR",
+        productId: id,
+        isWorkTrip: false
+    }
+    newQuery["numberOfGuests"] = newQuery.numberOfAdults + newQuery.numberOfChildren + newQuery.numberOfInfants
+
+    const url = `https://www.airbnb.fr/book/stays/${id}?${new URLSearchParams(newQuery).toString()}`
+
+    const html = await (await fetch(url)).text()
+
+    let splitIndex = html.indexOf('<script id="data-state"')
+    splitIndex = html.indexOf('>', splitIndex)
+    let contentText = html.substring(splitIndex + 1)
+    splitIndex = contentText.indexOf("</script>")
+    contentText = contentText.substring(0, splitIndex)
+
+    let data = JSON.parse(contentText)
+
+    contentText = data.niobeMinimalClientData[0][1].data.presentation.stayCheckout.sections.temporaryQuickPayData.bootstrapPaymentsJSON
+    data = JSON.parse(contentText)
+
+    const prices = data.productPriceBreakdown.priceBreakdown.priceItems
+    const result = {}
+    for (const price of prices) {
+        result[price.type.toLocaleLowerCase()] = price.total.amountMicros / 1e6
+    }
+    result["totalPrice"] = data.productPriceBreakdown.priceBreakdown.total.total.amountMicros / 1e6
+    return result
+}
+
+/**
+ * Scraps the room
+ * @returns parsed data
+ */
+const scrap = async () => {
+    log("Scraping starts...")
+    const params = parseUrl()
+
+    const api = new API(API_KEY)
+    const response = await api.getStaysPdpSection(params)
+    const data = await response.json()
+
+    const result = parseStaysPdpSection(data)
+
+    const priceResult = await getRealPrice(params)
+    for (const [key, value] of Object.entries(priceResult)) {
+        result[key] = value
+    }
+
+    return result
+}
+
+const button = Scrap.addScrapButton()
+button.onclick = () => {
+    scrap()
+        .then(result => {
+            log("RESULT", result)
+        })
+        .catch(error => {
+            console.error("Error while parsing", error)
+        })
+}
 
